02d970a6d967c04a280d3dc5ccd2b302a8a5be70
marcorosas1991 <marco.rosas@outlook.com>
2016-07-16 17:55:10 -0600

    create main page

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+
+  <body id="myPage">
+
+  <header>
+    <!-- Main logo -->
+    <div class="main_logo">
+      <a class="logo_font" href="index.php"><span class="logo_gray_part">Studi</span><span class="logo_red_part"> Vanzzini</span></a>
+    </div>
+
+    <!-- Navigation -->
+    <ul class="navigation">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </header>
+
+  <main>
+    <h1>Get started with your web presence.</h1>
+
+    <h2>Our services:</h2>
+    <ul class="card_container" id="main_services_section">
+      <li class="card">
+          <span class="card_title">Web pages</span>
+      </li>
+      <li class="card">
+          <span class="card_title">Boost your online presence</span>
+      </li>
+      <li class="card">
+          <span class="card_title">Web apps</span>
+      </li>
+    </ul>
+    <section id="main_clients_section">
+
+    </section>
+  </main>
+
+    <footer>
+
+    </footer>
+  </body>
+</html>
