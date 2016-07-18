cc54ee4b2c1dc49ec79bcbedb2b67cbad1cddf70
marcorosas1991 <marco.rosas@outlook.com>
2016-07-18 08:57:16 -0600

    text in index

--- index.php
+++ index.php
@@ -6,19 +6,37 @@
     <h1>Get started with your web presence.</h1>
 
     <h2>Our services:</h2>
-    <a href="services.php">
-    <ul class="card_container" id="main_services_section">
-      <li class="card">
-          <span class="card_title">Web pages</span>
-      </li>
-      <li class="card">
-          <span class="card_title">Web optimizer</span>
-      </li>
-      <li class="card">
-          <span class="card_title">Web apps</span>
-      </li>
-    </ul>
-    </a>
+      <ul class="card_container" id="main_services_section">
+        <li class="card">
+            <span class="card_title">Web pages</span>
+            <p>
+              For businesses that currently do not have a web presence.
+              We will design and create a stunning website for your company.
+              We will focus in the graphic design, and quality of it.
+              Design is crucial to make a good impression in your customers.
+            </p>
+            <a href="contact.php">Get a free consultation</a>
+        </li>
+        <li class="card">
+            <span class="card_title">Web optimizer</span>
+            <p>
+              For businesses that want to boost their existing online presence.
+              We will optimize your website to be found by search engines like Google, Bing, Yahoo, etc.
+              Being online is not enough, being able to find you quickly will increase your number of customers.
+            </p>
+            <a href="contact.php">Get a free consultation</a>
+        </li>
+        <li class="card">
+            <span class="card_title">Web apps</span>
+            <p>
+              For businesses that need a dynamic web solution.
+              We will design a custom app for your operations.
+              Web apps will allow your employees to access software from any browser.
+              Security and access levels can be configured.
+            </p>
+            <a href="contact.php">Get a free consultation</a>
+        </li>
+      </ul>
 
     <h2>Our clients:</h2>
     <ul class="card_container" id="main_clients_section">
